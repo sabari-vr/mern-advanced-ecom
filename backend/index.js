ef40b9445bf08e7b641a7ce6ce577ef151d7d3c8
Sabari VR <sabari.r@nergyindia.com>
2024-10-06 10:08:53 +0530

    vercel json

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 app.use(express.json({ limit: "15mb" })); // allows us to parse incoming requests:req.body
 
