9cb4b4ed5aeae4a1b0eb832f695aebd1debdeff3
Sabari VR <sabari.r@nergyindia.com>
2024-11-01 12:09:03 +0530

    new product card

--- frontend/src/master/components/ProductCard.jsx
+++ frontend/src/master/components/ProductCard.jsx
@@ -14,17 +14,17 @@ const ProductCard = ({ product, wishListMutation, wishListState }) => {
 
     return (
         <div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
-            <Link to={'/product/' + product._id}>
+            <Link to={'/product/' + product?._id}>
                 <div className='relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl'>
-                    <img className='object-cover w-full' src={product.images[0]} alt='product image' />
+                    <img className='object-cover w-full' src={product?.images[0]} alt='product image' />
                     <div className='absolute inset-0 bg-black bg-opacity-20' />
                 </div>
 
                 <div className='mt-4 px-5 pb-5'>
-                    <h5 className='text-xl font-semibold tracking-tight text-white'>{product.name}</h5>
+                    <h5 className='text-xl font-semibold tracking-tight text-white'>{product?.name}</h5>
                     <div className='mt-2 mb-5 flex items-center justify-between'>
                         <p>
-                            <span className='text-3xl font-bold text-emerald-400'>₹ {product.price}</span>
+                            <span className='text-3xl font-bold text-emerald-400'>₹ {product?.price}</span>
                         </p>
                     </div>
                 </div>
